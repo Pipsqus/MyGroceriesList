dba8c4cd218571abf923497126ec484ac949fbac
Pipsqus <andreaforconi@protonmail.com>
2021-06-28 09:18:18 +0200

    various changes to newgroceries

--- newGroceriesEdition/src/Navigation.js
+++ newGroceriesEdition/src/Navigation.js
@@ -11,17 +11,43 @@ function updateLists(shoppingCart) {
 	let listOfSelectedRecipes = document.getElementById("listOfSelectedRecipes");
 	listOfSelectedRecipes.innerHTML = "Ricette";
 	for (let key of Object.keys(shoppingCart.recipes)) {
-		let recipeItem = 
-			`<div><input type="radio" onclick="() => {
-				shoppingCart.recipes['${key}'] = 1;
-				}">${key}</div>`
-		listOfSelectedRecipes.insertAdjacentHTML('beforeend', recipeItem);
+		if (shoppingCart.recipes[key] === "completed") {
+			let recipeItem = 
+				`<div><input id="${key}Listed" type="radio" onclick=recipeWasExecuted('${key}') checked><label for="${key}Listed">${key}</label></input></div>`;
+			listOfSelectedRecipes.insertAdjacentHTML('beforeend', recipeItem);
+		} else {
+			let recipeItem = 
+				`<div><input id="${key}Listed" type="radio" onclick=recipeWasExecuted('${key}')><label for="${key}Listed">${key}</label></input></div>`;
+			listOfSelectedRecipes.insertAdjacentHTML('beforeend', recipeItem);
+		}
 	}
 
 	let listOfIngredients = document.getElementById("listOfIngredients");
 	listOfIngredients.innerHTML = "Ingredienti";
 	for (const [key, value] of Object.entries(shoppingCart.ingredients)) {
-		let ingredientItem = `<div><input type="radio">${key}: ${value}</div>`
-		listOfIngredients.insertAdjacentHTML('beforeend', ingredientItem);
+		if (shoppingCart.ingredients[key] === "acquired") {
+			let ingredientItem = `<div><input id="${key}Ingredient" type="radio" onclick=ingredientWasAcquired('${key}') checked><label for="${key}Ingredient">${key}</label></input></div>`
+			listOfIngredients.insertAdjacentHTML('beforeend', ingredientItem);
+		} else {
+			let ingredientItem = `<div><input id="${key}Ingredient" type="radio" onclick=ingredientWasAcquired('${key}')><label for="${key}Ingredient">${key}</label></input></div>`
+			listOfIngredients.insertAdjacentHTML('beforeend', ingredientItem);
+		}
 	}
 }
+
+function recipeWasExecuted(recipe) {
+	shoppingCart.recipes[recipe] = "completed";
+	localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+}
+function ingredientWasAcquired(ingredient) {
+	shoppingCart.ingredients[ingredient] = "acquired";
+	localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+}
+function resetLists() {
+	localStorage.clear();
+	document.getElementById("listOfSelectedRecipes").innerHTML = "Recipes";
+	document.getElementById("listOfIngredients").innerHTML = "Ingredienti";
+	shoppingCart = {recipes: {}, ingredients: {}};
+	window.location.reload();
+}
+
