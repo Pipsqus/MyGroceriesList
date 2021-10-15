b1036feba229c9a9530818cff86638f050ea82c0
Pipsqus <andreaforconi@protonmail.com>
2021-10-15 10:51:55 +0200

    DELETED THE OLD GROCERIES -> MIGRATED TO NEW

--- src/knownRecipes.js
+++ src/knownRecipes.js
@@ -23,6 +23,7 @@ const CATEGORY_HOLDER = {
 		"Pansoti con pesto alle noci" : ["erbette", "vino bianco", "ricotta", "noci", "pinoli", "rosmarino", ""],
 		"Pere, Ricotta, Zafferano, Burro" : ["ricotta", "zafferano", "burro", ""],
 		"Scarpinocc" : ["pane grattuggiato", "burro", ""],
+		"Cappellacci alla Zucca" : ["zucca", "burro", "salvia", "https://www.lospicchiodaglio.it/ricetta/cappellacci-di-zucca"],
 		"Tortellini" : ["carne macinata", "mortadella", "prosciutto crudo", ""],
 		"Stracciatella" : ["brodo", "pane grattuggiato", ""],
 		
@@ -30,7 +31,7 @@ const CATEGORY_HOLDER = {
 
 	"Panetteria" : 
 	{
-		"Crescia Estate" : ["strutto", "mortadella", "ricotta", ""],
+		"Crescia Estate" : ["strutto", "mortadella", "ricotta", "https://blog.giallozafferano.it/ricettevelociditania/piadina-sfogliata-marchigiana/"],
 		"Crescia Autunno" : ["spinaci", "funghi", "aceto balsamico", ""],
 		"Crescia Inverno" : ["cavolo rosso", "pinoli", "cipolle", "noci", ""],
 		"Crescia Provenzale" : ["tonno", "mozzarella", "pomodori", ""],
@@ -41,12 +42,12 @@ const CATEGORY_HOLDER = {
 		"Strangolapreti" : ["salvia", "burro", "pane", "bietola", ""],
 		"Canederli" : ["pane", "burro", "brodo", ""],
 		"Polenta Funghi e Gorgonzola" : ["polenta", "gorgonzola", "funghi", "vino bianco", ""],
-		"Pizzette di Sfoglia" : ["pomodori", "mozzarella", "burro", ""],
-		"Sfoglia Ricotta e Spinaci" : ["ricotta", "spinaci", ""],
+		"Pizzette di Sfoglia" : ["pomodori", "mozzarella", "burro", "my-recipes/sfoglia.html"],
+		"Sfoglia Ricotta e Spinaci" : ["ricotta", "spinaci", "my-recipes/sfoglia.html"],
 		"Pizza Margherita" : ["mozzarella", "pomodoro", ""],
 		"Pizza Cipolla e Tonno" : ["cipolla rossa", "tonno", "mozzarella", ""],
-		"Focaccia Cipolla e Gorgonzola" : ["cipolla", "gorgonzola", ""],
-		"Focaccia Pomodori e Olive" : ["pomodori", "olive", ""],
+		"Focaccia Cipolla e Gorgonzola" : ["cipolla", "gorgonzola", "https://www.tavolartegusto.it/ricetta/focaccia-morbida-ricetta-focaccia/"],
+		"Focaccia Pomodori e Olive" : ["pomodori", "olive", "https://www.tavolartegusto.it/ricetta/focaccia-morbida-ricetta-focaccia/"],
 	},
 
 	"Frittura" : {
@@ -78,18 +79,18 @@ const CATEGORY_HOLDER = {
 		"Lasagna Bianca" : [""],
 		"Bratwurst mit Sauerkraut" : ["Bratwurst", "Sauerkraut", ""],
 		"Alla Filippo" : ["vino rosso", "panna", "cipolle", "salsiccia", ""],
-		"Currywurst" :  ["Bratwurst", "pomodori", "brodo", "miele", "aceto di mele", ""],
+		"Currywurst" :  ["Bratwurst", "pomodori", "brodo", "miele", "aceto di mele", "https://www.daringgourmet.com/best-german-curry-ketchup-for-currywurst/"],
 	},
 
 	"Dolci" : {
-		"Waffles" : ["zucchero perlato", "burro", "lievito",  "https://blog.giallozafferano.it/inventaricette/waffel-gaufres-liege/"],
+		"Waffles" : ["zucchero perlato", "burro", "lievito",  "my-recipes/waffles.html"],
 		"Camille" : ["carote", "mandorle", "succo di arancia", ""],
 		"Ottolenghi Cookies" : ["pere", "cioccolato bianco", "limone", "https://www.theguardian.com/food/2018/nov/24/pear-recipes-cheesecake-choc-chip-cookie-ginger-cake-yotam-ottolenghi"],
 		"Pere e Vino" : ["pere", "vino rosso", "https://ricette.giallozafferano.it/pere-al-vino-rosso.html"],
 		"Cicerchiata" : ["miele di acacia", "vino liquoroso", "https://blog.giallozafferano.it/quasimagia/cicerchiata-ricetta-di-carnevale-marche/"],
 		"Panna Cotta" : ["panna", "agar agar", "frutti di bosco", ""],
 		"Pancakes" : [""],
-		"Milkshake" : ["fragole", "lamponi", "marmellata", "gelato fiordilatte", "https://www.theguardian.com/food/2020/aug/12/how-to-make-the-perfect-strawberry-milkshake-recipe-felicity-cloake"],
+		"Milkshake" : ["fragole", "lamponi", "marmellata", "gelato fiordilatte", "my-recipes/milkshake.html"],
 		"Cestini" : ["frutti di bosco", "https://www.misya.info/ricetta/cestini-di-frutta.htm"],
 		"Profiteroles" : ["panna fresca", "cioccolato fondente", "cioccolato bianco", "https://ricette.giallozafferano.it/Profiteroles-al-cioccolato-bianco.html"],
 		"Tiramisù" : ["mascarpone", "cacao amaro", "caffè", "savoiardi", ""],
